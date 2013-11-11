b431501627da88b72d9dda499d9d536921d7ede6
Jamie Phillips <jamie.phillips@spatialclastics.com>
2013-11-10 21:43:45 -0500

    Started tweaking the theme

---
+++ 2013/11/10/testing.rst
@@ -0,0 +1,10 @@
+Testing
+=======
+
+Just a test page.
+
+
+.. author:: default
+.. categories:: none
+.. tags:: none
+.. comments::
--- conf.py
+++ conf.py
@@ -8,7 +8,7 @@ import tinkerer.paths
 # **************************************************************
 
 # Change this to the name of your blog
-project = 'Spatialclastics'
+project = 'SpatialClastics'
 
 # Change this to the tagline of your blog
 tagline = ''
